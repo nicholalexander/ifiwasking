561e00eb4f2ae65dec64d224550a8c1ca6e5ea6c
Nichol Alexander <nichol.alexander@gmail.com>
2013-10-22 14:56:19 -0400

    using callback :generate_username to modify devise behavior courtesey of jeff

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,14 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  before_create :generate_username
+
   has_many :proclamations
+
+  private
+
+  def generate_username
+    binding.pry
+    self.username = self.email.split("@")[0]
+  end
 end
