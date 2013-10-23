341ce1521e2cb944551687de642813d8b69157f0
Nichol Alexander <nichol.alexander@gmail.com>
2013-10-23 13:27:16 -0400

    trying to clean up mistake edits of >>>>Head and added skeleton framework

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,30 +1,14 @@
 class VotesController < ApplicationController
 
   def index
-
-    binding.pry
-  end
-
-  def create
-
-
-    @votes = Vote.all
     @ranked = Proclamation.plusminus_tally
-  
   end
 
   def new
     @vote = Vote.new
-
   end
 
   def create
-    
-    @vote = Vote.new(vote_params)
-    @proclamation.user = current_user
-
-    redirect '/'
-
   end
 
 
